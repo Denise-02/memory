db38aa1e9d0443adf5e67cfdeb33247ec3249702
Denise-02 <denisebavieri@gmail.com>
2023-08-31 19:11:17 +0200

    provato a sistemare schermata di gioco :(

--- src/main/java/com/game/memory/GameController.java
+++ src/main/java/com/game/memory/GameController.java
@@ -1,34 +1,22 @@
 package com.game.memory;
 
-import javafx.animation.AnimationTimer;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.DialogPane;
+import javafx.geometry.HPos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.scene.layout.*;
 
 import java.util.*;
 import java.util.random.RandomGenerator;
 
 /* https://www.datainfinities.com/27/pass-event-and-parameter-onclick-in-react#:~:text=To%20pass%20event%20and%20parameter%20onClick%20in%20React%2C%20declare%20an,an%20element(eg%20button).
-* How do you pass an event as a parameter?
-* To pass event and parameter onClick in React, declare an event handler
-* function and then pass it to the onClick prop of the element inside an inline arrow
-* function. Using React onClick event handler you can call a function and trigger an
-* action when a user clicks an element(eg button).
-* */
+ * How do you pass an event as a parameter?
+ * To pass event and parameter onClick in React, declare an event handler
+ * function and then pass it to the onClick prop of the element inside an inline arrow
+ * function. Using React onClick event handler you can call a function and trigger an
+ * action when a user clicks an element(e.g. button).
+ * */
 
 public class GameController {
     // 4 x 2    --> livello 1: 4 immagini
@@ -45,19 +33,9 @@ public class GameController {
     @FXML
     private Label lblLevel;
 
-    @FXML private AnchorPane anchorPane;
     Game game;
 
-    @FXML private HBox hBox;
-
-
-
-    @FXML private ImageView imageView;
-
-
-    private String theme;
-    private int mode;
-    private int level;
+    private final int NCOLS = 4;
 
     /**
      * The level of the game. For each level the number of cards increases.
@@ -65,9 +43,7 @@ public class GameController {
     @FXML
     public void initialize() {
         game = new Game("", 1, 1);
-     //   gridPane.setGridLinesVisible(true);
-        anchorPane = new AnchorPane();
-        anchorPane.setStyle("fx-background-color: pink");
+        gridPane.setGridLinesVisible(true);
 /*
         hBox = new HBox(15);
         hBox.setPadding(new Insets(50, 150, 50, 60));
@@ -75,6 +51,7 @@ public class GameController {
 */
 
     }
+
     public void receiveData(String theme, int mode, int level) {
         game.setTheme(theme);
         game.setMode(mode);
@@ -85,24 +62,19 @@ public class GameController {
 
     @FXML
     void update() {
-        theme = game.getTheme();
-        mode = game.getMode();
-        level = game.getLevel();
-        System.out.println("I say the theme is: " + theme);
-
-        lblTheme.textProperty().set("" + theme);
-        lblMode.textProperty().set("" + mode);
-        lblLevel.textProperty().set("" + level);
+        lblTheme.textProperty().set("" + game.getTheme());
+        lblMode.textProperty().set("" + game.getMode());
+        lblLevel.textProperty().set("" + game.getLevel());
 
         initializeGame();
     }
 
 
-  @FXML
+    @FXML
     private void initializeGame() {
         //  ( liv x 2 + 2 ) x 2
         // controllo sul livello per sapere quanti elementi conterrà l'array e quante righe aggiungere
-
+        //  game.setLevel(4);
         HashSet<Integer> cards = new HashSet<>();
         ArrayList<Integer> cardCouples = new ArrayList<>();
         System.out.println("The level is: " + game.getLevel());
@@ -113,16 +85,18 @@ public class GameController {
 
         System.out.println("The length is: " + length);
         int j = 0;
-        while (j < length){
-            if (cards.add(randomGenerator.nextInt(1, 11))){
+        while (j < length) {
+            if (cards.add(randomGenerator.nextInt(1, 11))) {
                 j++;
             }
         }
+
 
         cardCouples.addAll(cards);
         cardCouples.addAll(cards);
 
         Collections.shuffle(cardCouples);
+
 
         System.out.println(Arrays.toString(cardCouples.toArray()));
         for (int i = 1; i <= game.getLevel(); ++i) {
@@ -131,19 +105,50 @@ public class GameController {
             gridPane.addRow(i, new StackPane(), new StackPane(), new StackPane(), new StackPane());
         }
 
+        // numero cols = 4
+        // numero rows = game.getLevel() + 1
+
+
+        RowConstraints rowConstraints = new RowConstraints();
+        rowConstraints.setPercentHeight(100d / (game.getLevel() + 1));
+        rowConstraints.vgrowProperty().set(Priority.valueOf("SOMETIMES"));
+        rowConstraints.minHeightProperty().set(10.0);
+        rowConstraints.prefHeightProperty().set(30.0);
+  /*    for (int rows = 0; rows < game.getLevel() + 1; ++rows) {
+          gridPane.getRowConstraints().add(rowConstraints);
+      }
+*/
+
         int count = 0;
-        for (int cols = 0; cols < 4; ++cols) {    // 4 costante e
-          for (int rows = 0; rows < game.getLevel() + 1; ++rows) {
+        for (int cols = 0; cols < NCOLS; ++cols) {
+            for (int rows = 0; rows < game.getLevel() + 1; ++rows) {
+                //     gridPane.getRowConstraints().add(rowConstraints);
                 System.out.println("memoryImages/" + game.getTheme() + "/" + cardCouples.get(count) + ".jpg");
-                Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("memoryImages/" + game.getTheme() + "/" + cardCouples.get(count) + ".jpg")), 200, 200, true, true);
+                Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("memoryImages/" + game.getTheme() + "/" + cardCouples.get(count) + ".jpg")), 90, 90, true, true);
                 ImageView imageView1 = new ImageView();
                 imageView1.setImage(image);
 
                 gridPane.add(imageView1, cols, rows);
-              ++count;
+                GridPane.setHalignment(imageView1, HPos.CENTER);
 
-          }
+
+                ++count;
+            }
+
         }
+  /*    GridPane.setFillHeight(gridPane, true);
+      GridPane.setFillWidth(gridPane, true);
+*/
+        // gridPane.getChildren().
+    /*    double gridHeight = gridPane.getHeight();
+        System.out.println("gridHeight" + gridHeight);
+        double gridWidth = gridPane.getWidth();
+      System.out.println("gridWidth" + gridWidth);*/
+        //  gridPane.setPrefHeight(gridWidth);
+        //   gridPane.getRowConstraints().add(new RowConstraints(gridWidth / (game.getLevel() + 1))); // column 1 is 200 wide
+
+
+
 /* a
         // timer
         Timer timer = new Timer();
@@ -156,10 +161,12 @@ public class GameController {
 */
 
     }
-
+/*
   public Game getGame() { return game; }
   public void setGame(Game game) {
      this.game = game;
     // update();
   }
+  */
+
 }
